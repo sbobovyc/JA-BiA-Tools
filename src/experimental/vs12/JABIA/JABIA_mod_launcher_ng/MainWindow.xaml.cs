5c195624d680ef782576826eaa11b5b148369931
Stan Bobovych <stan.bobovych@gmail.com>
2014-05-02 21:02:30 -0400

    * Modified mod launcher to pause game thread to inject DLLs and resume thread

--- src/experimental/vs12/JABIA/JABIA_mod_launcher_ng/MainWindow.xaml.cs
+++ src/experimental/vs12/JABIA/JABIA_mod_launcher_ng/MainWindow.xaml.cs
@@ -16,10 +16,12 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 using System;
+using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -35,6 +37,75 @@ using DllInjection;
 namespace JABIA_mod_launcher_ng
 {
     enum GameVersion { JABIA, JAC, UNKNOWN };
+    public static class NativeMethods
+    {
+        [DllImport("kernel32.dll")]
+        public static extern bool CreateProcess(string lpApplicationName,
+               string lpCommandLine, IntPtr lpProcessAttributes,
+               IntPtr lpThreadAttributes,
+               bool bInheritHandles, ProcessCreationFlags dwCreationFlags,
+               IntPtr lpEnvironment, string lpCurrentDirectory,
+               ref STARTUPINFO lpStartupInfo,
+               out PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint ResumeThread(IntPtr hThread);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint SuspendThread(IntPtr hThread);
+    }
+
+    public struct STARTUPINFO
+    {
+        public uint cb;
+        public string lpReserved;
+        public string lpDesktop;
+        public string lpTitle;
+        public uint dwX;
+        public uint dwY;
+        public uint dwXSize;
+        public uint dwYSize;
+        public uint dwXCountChars;
+        public uint dwYCountChars;
+        public uint dwFillAttribute;
+        public uint dwFlags;
+        public short wShowWindow;
+        public short cbReserved2;
+        public IntPtr lpReserved2;
+        public IntPtr hStdInput;
+        public IntPtr hStdOutput;
+        public IntPtr hStdError;
+    }
+
+    public struct PROCESS_INFORMATION
+    {
+        public IntPtr hProcess;
+        public IntPtr hThread;
+        public uint dwProcessId;
+        public uint dwThreadId;
+    }
+
+    [Flags]
+    public enum ProcessCreationFlags : uint
+    {
+        ZERO_FLAG = 0x00000000,
+        CREATE_BREAKAWAY_FROM_JOB = 0x01000000,
+        CREATE_DEFAULT_ERROR_MODE = 0x04000000,
+        CREATE_NEW_CONSOLE = 0x00000010,
+        CREATE_NEW_PROCESS_GROUP = 0x00000200,
+        CREATE_NO_WINDOW = 0x08000000,
+        CREATE_PROTECTED_PROCESS = 0x00040000,
+        CREATE_PRESERVE_CODE_AUTHZ_LEVEL = 0x02000000,
+        CREATE_SEPARATE_WOW_VDM = 0x00001000,
+        CREATE_SHARED_WOW_VDM = 0x00001000,
+        CREATE_SUSPENDED = 0x00000004,
+        CREATE_UNICODE_ENVIRONMENT = 0x00000400,
+        DEBUG_ONLY_THIS_PROCESS = 0x00000002,
+        DEBUG_PROCESS = 0x00000001,
+        DETACHED_PROCESS = 0x00000008,
+        EXTENDED_STARTUPINFO_PRESENT = 0x00080000,
+        INHERIT_PARENT_AFFINITY = 0x00010000
+    }
 
     public class Mod
     {
@@ -53,7 +124,7 @@ namespace JABIA_mod_launcher_ng
 
     public class Settings
     {
-        public String version = "1.01";
+        public String version = "1.02";
         public List<Mod> mods = new List<Mod>();
         public List<string> modPaths;       
     }
@@ -153,10 +224,25 @@ namespace JABIA_mod_launcher_ng
                 ModListBox.IsEnabled = false;
             });
 
+            IntPtr ThreadHandle = IntPtr.Zero;
             if (version == GameVersion.JABIA)
-                System.Diagnostics.Process.Start(JABIA_LAUNCHER);
+            {
+                //System.Diagnostics.Process.Start(JABIA_LAUNCHER);
+                STARTUPINFO si = new STARTUPINFO();
+                PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+                bool success = NativeMethods.CreateProcess(JABIA_PROCESS+".exe", null,
+                    IntPtr.Zero, IntPtr.Zero, false,
+                    ProcessCreationFlags.CREATE_SUSPENDED,
+                    IntPtr.Zero, null, ref si, out pi);
+                ThreadHandle = pi.hThread;
+                NativeMethods.ResumeThread(ThreadHandle); 
+                Thread.Sleep(1000);
+                NativeMethods.SuspendThread(ThreadHandle);                               
+            }
             else
+            {
                 System.Diagnostics.Process.Start(JAC_LAUNCHER);
+            }
 
             if (version == GameVersion.JABIA)
             {                
@@ -225,6 +311,7 @@ namespace JABIA_mod_launcher_ng
                 }
                 
             }
+            NativeMethods.ResumeThread(ThreadHandle); 
             System.Threading.Thread.Sleep(1000);
             Process.GetCurrentProcess().Kill(); //TODO nasty, fix
         }
