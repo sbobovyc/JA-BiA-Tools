76a119b408a44fd0f3b757e3677724b0c0c0c018
Stan Bobovych <stan.bobovych@gmail.com>
2014-10-16 00:19:56 -0700

    Weapon parser mostly working, need to fix handling of strings such as "9_mm"

--- src/experimental/jabia_config_parser2.py
+++ src/experimental/jabia_config_parser2.py
@@ -1,9 +1,10 @@
 import pprint
 from pyparsing import \
-    Literal,Word,alphas,nums,OneOrMore,ZeroOrMore,Group,cppStyleComment,Combine,Optional,Suppress,LineEnd,LineStart,delimitedList,dblQuotedString
+    Keyword,Literal,Word,alphas,nums,alphanums,MatchFirst,OneOrMore,ZeroOrMore,ParserElement,ParseExpression, \
+    Group,cppStyleComment,Combine,Optional,Suppress,LineEnd,LineStart,delimitedList,dblQuotedString,restOfLine
 
 def convertNumbers(s,l,toks):
-    n = toks[0]
+    n = toks[0]    
     try:
         return int(n)
     except ValueError, ve:
@@ -20,87 +21,45 @@ quote = Literal("\"").suppress()
 newline = Suppress(LineEnd())
 comment = cppStyleComment
 
+                   
+
+classKeywords = map(lambda x: Keyword(x), ["Weapon", "Attachment"])
+weaponKeywords = map(lambda x: Keyword(x), ["Weight", "Price", "ResourceId",
+                                            "ShotEffectId", "Damage", "BestRange",
+                                            "StanceFactor", "Burst", "Auto",
+                                            "RPM", "ClipSize", "GunType",
+                                            "Ammunition", "Quality", "Icon",
+                                            "Picture", "DisableAttachments","AnchorPoint",
+                                            "Deliverable"
+                                            ])
+reservedKeywords = MatchFirst(weaponKeywords)
+integer = Word(nums)
 number = Combine( Optional('-') + ( '0' | Word('123456789',nums) ) +
-                    Optional( '.' + Word(nums) ) ) 
-value = number | Word(alphas)                     
-statementDef = Group ( Word(alphas) + OneOrMore( number ) ) + newline 
+                    Optional( '.' + integer ) ) 
+value = ~reservedKeywords + (number | Word(alphanums) | Word(alphanums + '_' + alphanums))
+print nums
+print reservedKeywords
+statementDef =  Group ( reservedKeywords + Optional( ZeroOrMore(value) ) )
 bodyDef = ZeroOrMore( statementDef )
-#classObj = Group( Word(alphas) + lparan + Word(nums) + comma + Word(alphas) + Optional( Word(nums) ) + rparan )
-classObj = Group( Word(alphas) + lparan + delimitedList(Word(nums) ^ Word(alphas) ^ Combine(Word(alphas) + Literal(" ") + Word(nums)) )  + rparan )
-classDef = classObj + lbrace + bodyDef + rbrace
-classDef.ignore(comment)
-classDef.ignore(quote)
+classObj = Group( MatchFirst(classKeywords) + lparan + delimitedList(integer ^ Word(alphas) ^ Combine(Word(alphas) + Literal(" ") + integer) )  + rparan )
+classDef =  Group( classObj + lbrace + bodyDef + rbrace )
+configFileDef = ZeroOrMore(classDef)
+configFileDef.ignore(comment)
+configFileDef.ignore(quote)
 number.setParseAction( convertNumbers )
+integer.setParseAction( convertNumbers )
 
-testString = """Class (45, id 53, "etst") {
-//this is a comment
-TestA 3 2
-TestB 5.3 -6.6
-}"""
 
-testString2 = """
-// SCAR-L Urban
-Weapon (70, Rifle) {
-Weight 400
-Price 400
-ResourceId 6071
-ShotEffectId 538
 
-// logic:
-Damage 98
-BestRange 125.0
-StanceFactor 0 2.1
-StanceFactor 1 2.9
-StanceFactor 2 2.65
-StanceFactor 3 2.8
-Burst 3
-Auto 30
-RPM 9625
-ClipSize 30
-//GunType AssaultRifle
-//Ammunition 50cal
-//Quality 9850
-}
-"""
+filename = "weapons.txt"
+f = open(filename)
+testString = f.read()
+#print testString
+parsed = configFileDef.parseString(testString)
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint( parsed.asList() )
 
-testString3 = """
-// SCAR-L Urban
-Weapon (70, Rifle)
-{
-Weight 400
-Price 400
-ResourceId 6071
-ShotEffectId 538
 
-// logic:
-Damage 98
-BestRange 125.0
-StanceFactor 0 2.1
-StanceFactor 1 2.9
-StanceFactor 2 2.65
-StanceFactor 3 2.8
-Burst 3
-Auto 30
-RPM 9625
-ClipSize 30
-GunType AssaultRifle
-Ammunition 50cal
-Quality 9850
 
-Icon 0 448 800 112 40
-Picture 8 3 4 3
-
-AnchorPoint 0.0 1.1 0.25
-
-Deliverable
-
-}
-"""
-pprint.pprint( classDef.parseString(testString) )
-
-#filename = "weapons.txt"
-#f = open(filename)
-#text = f.read()
-#classDef.parseString(text)
 
 
